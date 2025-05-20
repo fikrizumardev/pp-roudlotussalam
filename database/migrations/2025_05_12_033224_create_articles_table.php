93201d1d448cf8a5b41b3ea6b4fe0fc7995f2231
Fikri Zumar <muhammad.fikri.azzumardi@gmail.com>
2025-05-20 19:50:30 +0700

    Tambahan tabel artikel dan kategori

---
+++ database/migrations/2025_05_12_033224_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->string('cover_path')->nullable();
+            $table->foreignId('author_id')->references('id')->on('users');
+            $table->enum('status', ['draft', 'publish', 'archive'])->default('draft');
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
