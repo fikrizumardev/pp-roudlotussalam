dda5382a8b4a0b4fe3607d7e66a225f5112b4711
Fikri Zumar <muhammad.fikri.azzumardi@gmail.com>
2025-05-20 20:01:52 +0700

    Deklarasi package sweetalert di app js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+import Swal from 'sweetalert2'
+
+window.Swal = Swal
+
+// import tinymce from 'tinymce/tinymce';
+// import 'tinymce/icons/default';
+// import 'tinymce/themes/silver';
+// import 'tinymce/plugins/link';
+// import 'tinymce/plugins/lists';
+// import 'tinymce/plugins/image';
+// import 'tinymce/plugins/preview';
